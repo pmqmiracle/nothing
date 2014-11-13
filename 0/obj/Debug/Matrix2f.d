23e1bba5a1569b01d49fd91ebb08400d1e279525
pmqmiracle <pmqmiracle@foxmail.com>
2014-11-13 21:11:21 +0800

    MIT6.837 Assignment1

--- 0/obj/Debug/Matrix2f.d
+++
@@ -1,6 +0,0 @@
-obj/Debug/Matrix2f.o: vecmath/Matrix2f.cpp include/vecmath/Matrix2f.h \
- include/vecmath/Vector2f.h
-
-include/vecmath/Matrix2f.h:
-
-include/vecmath/Vector2f.h:
--- 0/obj/Debug/Vector3f.d
+++
@@ -1,6 +0,0 @@
-obj/Debug/Vector3f.o: vecmath/Vector3f.cpp include/vecmath/Vector3f.h \
- include/vecmath/Vector2f.h
-
-include/vecmath/Vector3f.h:
-
-include/vecmath/Vector2f.h:
