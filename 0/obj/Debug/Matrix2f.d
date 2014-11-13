ace34493ee50acf9af2e9dd3d6c206680045a73a
pmqmiracle <pmqmiracle@foxmail.com>
2014-11-13 21:03:25 +0800

    add MIT6.837 Assignment 0 and 1

---
+++ 0/obj/Debug/Matrix2f.d
@@ -0,0 +1,6 @@
+obj/Debug/Matrix2f.o: vecmath/Matrix2f.cpp include/vecmath/Matrix2f.h \
+ include/vecmath/Vector2f.h
+
+include/vecmath/Matrix2f.h:
+
+include/vecmath/Vector2f.h:
---
+++ 0/obj/Debug/Quat4f.d
@@ -0,0 +1,11 @@
+obj/Debug/Quat4f.o: vecmath/Quat4f.cpp include/vecmath/Quat4f.h \
+ include/vecmath/Matrix3f.h include/vecmath/Vector3f.h \
+ include/vecmath/Vector4f.h
+
+include/vecmath/Quat4f.h:
+
+include/vecmath/Matrix3f.h:
+
+include/vecmath/Vector3f.h:
+
+include/vecmath/Vector4f.h:
