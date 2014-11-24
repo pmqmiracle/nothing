ac2b0e4067733be1cad8a03a2f13938de632406d
pmqmiracle <pmqmiracle@foxmail.com>
2014-11-24 21:33:59 +0800

    MIT assignment 5, initial state

---
+++ 5/Group.h
@@ -0,0 +1,70 @@
+#ifndef GROUP_H
+#define GROUP_H
+
+#include "Object3D.h"
+#include "Ray.h"
+#include "Hit.h"
+#include <iostream>
+#include <vector>
+
+using  namespace std;
+
+///TODO:
+///Implement Group
+///Add data structure to store a list of Object*
+class Group:public Object3D
+{
+public:
+
+  Group(){
+      objectSize = 0;
+      //group.clear();
+  }
+
+  Group( int num_objects ){
+      objectSize = num_objects;
+      for(int i = 0;i < num_objects;++i)
+          group.push_back(NULL);
+  }
+
+  ~Group(){
+      //objectSize = 0;
+      //group.clear();
+  }
+
+  virtual bool intersect( const Ray& r , Hit& h , float tmin )
+  {
+      bool flag = false;
+      int whichOne = -1;
+      for(int i = 0;i < objectSize;++i)
+      {
+          if(group[i]->intersect(r,h,tmin))
+          {
+              flag = true;
+              whichOne = i;
+          }
+      }
+      //if(flag)
+        //cout << "intersect with " << whichOne << endl;
+      return flag;
+  }
+
+  //miracle : index for what???
+  void addObject( int index , Object3D* obj )
+  {
+      //group.push_back(obj);
+      this->group[index] = obj;
+  }
+
+  int getGroupSize()
+  {
+      return objectSize;
+  }
+
+private:
+  int objectSize;
+  vector<Object3D*> group;
+};
+
+#endif
+
---
+++ 5/Transform.h
@@ -0,0 +1,53 @@
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+
+#include <vecmath.h>
+#include "Object3D.h"
+#include "VecUtils.h"
+///TODO implement this class
+///So that the intersect function first transforms the ray
+///Add more fields as necessary
+class Transform: public Object3D
+{
+public:
+  Transform(){}
+  Transform( const Matrix4f& m, Object3D* obj ):o(obj)
+  {
+      this->M = m;//matrix
+  }
+  ~Transform()
+  {
+  }
+  virtual bool intersect( const Ray& r , Hit& h , float tmin)
+  {
+      //transform the ray from world space to object space
+      Matrix4f Minverse = this->M.inverse();
+      Vector3f newO = VecUtils::transformPoint(Minverse, r.getOrigin());
+      Vector3f newD = VecUtils::transformDirection(Minverse, r.getDirection());
+      //bug
+      //newD.normalized();
+      newD.normalize();
+
+      Ray newR(newO, newD);
+      //////////////////////////////////////////////
+      //转换完ray后,才开始intersect routine
+      if(o->intersect( newR , h , tmin))
+      {
+            //correctly transform the resulting normal
+            Vector3f newNormal = this->M.getSubmatrix3x3(0,0).inverse().transposed()*h.getNormal();
+            //bug
+            //newNormal.normalized();
+            newNormal.normalize();
+
+            h.set(h.getT(), h.getMaterial(), newNormal);
+            return true;
+      }
+      return false;
+  }
+
+protected:
+  Object3D* o; //un-transformed object
+  Matrix4f M;
+};
+
+#endif //TRANSFORM_H
